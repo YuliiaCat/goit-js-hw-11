39d9b3ceef5c89ffbdedb1d67214b42ed0518a8a
Yuliia <kotyuliya24@gmail.com>
2024-06-13 11:06:04 -0400

    finish

---
+++ src/main.js
@@ -0,0 +1,56 @@
+'use strict';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { fetchImages } from "./js/pixabay-api";
+import { renderImage } from "./js/render-functions";
+
+const btn = document.querySelector('.search-btn');
+const form = document.querySelector('.search-form');
+const input = document.querySelector('.search-input');
+const list = document.querySelector('.gallery');
+const loader = document.querySelector('.loader')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const value = event.target.elements.query.value.trim();
+  loader.classList.toggle('visually-hidden');
+
+  if (value === '') {
+    btn.disabled = true;
+    iziToast.error({
+      position: "topRight",
+      message: 'Please enter some text',
+    });
+  }
+
+  fetchImages(value)
+    .then(data => {
+      if (data.hits && data.hits.length > 0) {
+        const markup = renderImage(data.hits);
+        list.insertAdjacentHTML("beforeend", markup);
+        loader.classList.toggle('visually-hidden');
+
+        let lightbox = new SimpleLightbox('.gallery a', {
+          captions: true,
+          captionDelay: 250,
+          captionPosition: 'bottom',
+          captionsData: 'alt',
+        });
+
+        lightbox.refresh();
+      } else {
+          iziToast.error({
+            position: "topRight",
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+          })
+
+          loader.classList.toggle('visually-hidden');
+        }
+    })
+    .catch(err => {});
+
+  input.value = '';
+  list.innerHTML = '';
+});
